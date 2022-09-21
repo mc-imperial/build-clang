f7b9637256ca1a849cc188ec46269cfe1168d1ed
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-09-21 20:42:55 +0100

    Squash.

--- build.sh
+++ build.sh
@@ -20,6 +20,9 @@ set -u
 
 WORK="$(pwd)"
 
+# Old bash versions can't expand empty arrays, so we always include at least this option.
+CMAKE_OPTIONS=("-DCMAKE_OSX_ARCHITECTURES=x86_64")
+
 help | head
 
 uname
@@ -58,11 +61,19 @@ COMMIT_ID="$(cat "${WORK}/COMMIT_ID")"
 
 INSTALL_DIR="build-clang-${COMMIT_ID}-${BUILD_PLATFORM}_${CONFIG}"
 
+if [ -n "${USE_SANITIZER}" ]
+then
+    INSTALL_DIR="${INSTALL_DIR}-${USE_SANITIZER}-Sanitizer"
+fi
+
 export PATH="${HOME}/bin:$PATH"
 
 mkdir -p "${HOME}/bin"
 
 pushd "${HOME}/bin"
+
+# Install github-release-retry.
+"${PYTHON}" -m pip install --user 'github-release-retry==1.*'
 
 # Install ninja.
 curl -fsSL -o ninja-build.zip "https://github.com/ninja-build/ninja/releases/download/v1.9.0/ninja-${NINJA_OS}.zip"
@@ -74,7 +85,6 @@ popd
 
 CMAKE_GENERATOR="Ninja"
 CMAKE_BUILD_TYPE="${CONFIG}"
-CMAKE_OPTIONS="TODO"
 
 git clone https://github.com/llvm/llvm-project.git
 cd llvm-project
@@ -82,9 +92,36 @@ git checkout "${COMMIT_ID}"
 
 BUILD_DIR="b_${CONFIG}"
 
-mkdir -p "${BUILD_DIR}"
-cd "${BUILD_DIR}"
+CMAKE_OPTIONS+=("-DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra'" "-DLLVM_TARGETS_TO_BUILD=X86" "-DLLVM_ENABLE_ZSTD=OFF")
 
-cmake ../llvm -G "${CMAKE_GENERATOR}" "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" "-DLLVM_ENABLE_PROJECTS='clang'"
+if [ -n "${USE_SANITIZER}" ]
+then
+    CMAKE_OPTIONS+=("-DLLVM_USE_SANITIZER=${USE_SANITIZER}")
+fi
+
+mkdir -p "${BUILD_DIR}"
+pushd "${BUILD_DIR}"
+cmake ../llvm -G "${CMAKE_GENERATOR}" "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" "${CMAKE_OPTIONS[@]}"
 cmake --build . --config "${CMAKE_BUILD_TYPE}"
 cmake "-DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}" "-DBUILD_TYPE=${CMAKE_BUILD_TYPE}" -P cmake_install.cmake
+popd
+
+# zip file.
+pushd "${INSTALL_DIR}"
+zip -r "../${INSTALL_DIR}.zip" ./*
+popd
+
+# We do not use the GITHUB_TOKEN provided by GitHub Actions.
+# We cannot set enviroment variables or secrets that start with GITHUB_ in .yml files,
+# but the github-release-retry tool requires GITHUB_TOKEN, so we set it here.
+export GITHUB_TOKEN="${GH_TOKEN}"
+
+DESCRIPTION="$(echo -e "Automated build for llvm-project version ${COMMIT_ID}.")"
+
+"${PYTHON}" -m github_release_retry.github_release_retry \
+  --user "mc-imperial" \
+  --repo "build-clang" \
+  --tag_name "github/mc-imperial/build-clang/${COMMIT_ID}" \
+  --target_commitish "${GITHUB_SHA}" \
+  --body_string "${DESCRIPTION}" \
+  "${INSTALL_DIR}.zip"
