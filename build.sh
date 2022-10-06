c15e7a5df509dfcecc69d5705cff23b7400976e9
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-10-06 09:52:12 +0100

    Tweak.

--- build.sh
+++ build.sh
@@ -38,11 +38,11 @@ case "$(uname)" in
   tar xf clang+llvm.tar.xz
   mv clang+llvm-13.0.1-x86_64-linux-gnu-ubuntu-18.04 clang+llvm
   rm clang+llvm.tar.xz
-  echo ${WORK}
+  echo "${WORK}"
   ls ${WORK}
   ls ${WORK}/clang+llvm
   ls ${WORK}/clang+llvm/bin
-  export PATH=${WORK}/clang+llvm/bin:$PATH
+  export PATH="${WORK}/clang+llvm/bin":$PATH
   export CC=clang-13
   export CXX=clang++-13
 
